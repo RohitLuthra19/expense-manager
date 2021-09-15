bd9e1200ab0025547395e709e3b5a31d9c379227
Rohit Luthra <rohit.luthra19@gmail.com>
2021-09-15 19:33:08 +0530

    folder restructured

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./global.css";
 import "./normalize.css";
-import ExpenseManager from "./components/pages/expense-manager";
+import ExpenseManager from "./pages/expense-manager";
 
 ReactDOM.render(
   <React.StrictMode>
